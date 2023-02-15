7ee4f2cb42ffd531e6d42413f7fd577e968eabb3
Eduardo <edu.he.dev@gmail.com>
2023-02-15 01:31:12 -0300

    postcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,7 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-
-  const handleMouseUp = () => {
-    console.log(`Selected text: ${window.getSelection().toString()}`);
-  };
-  return (
-    <>
-      <div onMouseUp={handleMouseUp}>Textasdasddad</div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
